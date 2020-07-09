6f5a69b6034ad26d9e97370a7f65aab6c62e896b
gusadi <gusadi872@gmail.com>
2020-07-09 23:30:18 +0800

    Initial Commit

---
+++ GamesUp!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GamesUp!
+//
+//  Created by Agus Adi Pranata on 09/07/20.
+//  Copyright © 2020 gusadi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
